4e4a72b6bda753c27741253001bcda285fef9273
jeetesh <release123>
2024-07-28 14:02:38 +0530

    day26

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="font-bold text-3xl text-red-600">hello world netflix</div>
+  );
 }
 
 export default App;
